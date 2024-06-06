433df6d79fb2a76602917b2848e4c6508e93522c
Alishaw99 <tariqham@gmail.com>
2024-06-06 10:09:29 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Tariq Ali',
+    author_email= 'tariqham@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
